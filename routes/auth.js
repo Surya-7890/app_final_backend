54c9496c96496f1c2be7da32920bc166dbd08668
Surya-7890 <suryamariappan9108@gmail.com>
2023-09-30 15:48:16 +0530

    updated callback url

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 router.get('/login/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
 
 router.get('/login/google/callback', passport.authenticate('google', { session: false }), (req, res) => {
-  res.redirect(`sece://booking/${JSON.stringify(req.user)}`)
+  res.redirect(`sece://booking/${req?.user?.accessToken}/${req?.user?.refreshToken}/${req?.user?.role}`)
 });
 
 module.exports = router;
