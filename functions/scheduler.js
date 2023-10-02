2d611a677d537b2597b2fff224e38900028dc156
Surya-7890 <suryamariappan9108@gmail.com>
2023-10-02 21:28:19 +0530

    updated scheduler

--- functions/scheduler.js
+++ functions/scheduler.js
@@ -1,6 +1,7 @@
 const { months } = require('../utils/month')
 
 const scheduler = async (room, allocatedTime) => {
+  console.log(allocatedTime);
   const from = allocatedTime.split('to')[0]
     const to = allocatedTime.split('to')[1]
     let [ fromDate, fromMonth, fromYear, fromTime] = from.split(' ');
@@ -17,6 +18,7 @@ const scheduler = async (room, allocatedTime) => {
     // splitting time string
     const [fromHour, fromMinute] = fromTime.split(':');
     const [toHour, toMinute] = toTime.split(':');
+    console.log(fromTime, toTime);
     room.AddScheduler({
         hour: Number(toHour),
         minute: Number(toMinute),
